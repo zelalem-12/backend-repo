b0b53519fb985fa3794cb602769a4944d3760774
zelalem-12 <zelalem.antigegn12@gmail.com>
2021-10-12 15:27:52 +0300

    adding crose origin

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from "./app.module";
 
 (async () => {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ credentials: true, origin: "*" });
   await app.listen(5000);
 })();
